5079a26cbc4c6666ba51bedb971efd9838b6a9a4
M.U$man <92122488+musmanmazhar@users.noreply.github.com>
2021-11-15 18:43:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,190 @@
+const BtnAllusers = document.getElementById("getusers");
+const Btnuser = document.getElementById("getuser-id");
+const Btncreate = document.getElementById("postuser");
+//const BtnEdit = document.getElementById("putuser-id");
+const BtnDel = document.getElementById("deluser-id");
+const BtnDelAll = document.getElementById("delallusers");
+
+BtnAllusers.addEventListener("click", () => {
+  document.getElementById("tbod").textContent = ""
+  axios
+    .get("https://crud-express-mongodb-basic.herokuapp.com/users")
+    .then((response) => {
+      dataUsers = response.data;
+      console.log(dataUsers);
+       if (dataUsers.length === 0) {
+        alert("Please Create a User first!!");
+      } else {
+        for (i = 0; i < dataUsers.length; i++) {
+          //This loop, will loop over the all the available users.
+          //console.log(dataUsers[i]); //will give us all the available users in console
+
+          let iddata = dataUsers[i]
+          //console.log(iddata);
+
+          //console.log(dataUsers[i]._id);
+
+
+          let trUsers = document.createElement("tr");
+          let td1 = document.createElement("td");
+          let td2 = document.createElement("td");
+          let td3 = document.createElement("td");
+
+ 
+          var btn1edit = document.createElement('button')
+          btn1edit.textContent = "EDIT"
+          btn1edit.addEventListener('click', function() { EDIT(iddata._id) })
+
+          var btn2delete = document.createElement('button')
+          btn2delete.textContent = "DELETE"
+          //btn2delete.onclick = () => { DELETE(dataUsers[i]._id) } 
+          btn2delete.onclick = function () { DELETE(iddata._id) }
+
+          let td4 = document.createElement("td")
+          let td5 = document.createElement("td")
+
+
+          td1.appendChild(document.createTextNode(dataUsers[i].name));
+          td2.appendChild(document.createTextNode(dataUsers[i].email));
+          td3.appendChild(document.createTextNode(dataUsers[i].address));
+          td4.appendChild(btn1edit)
+          td5.appendChild(btn2delete)
+
+
+          trUsers.appendChild(td1);
+          trUsers.appendChild(td2);
+          trUsers.appendChild(td3);
+          trUsers.appendChild(td4);
+          trUsers.appendChild(td5);
+          document.getElementById("tbod").appendChild(trUsers);
+          //console.log(dataUsers[i]._id);
+        }
+
+      }
+      //console.log(ID);
+      //console.log(response.data); giving us the response data means the empty bracket
+      //console.log(dataUsers[0]); giving us the first user, which have index 0.
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+Btncreate.addEventListener("click", () => {
+  let inpName = document.getElementById("crN").value;
+  let inpEmail = document.getElementById("crE").value;
+  let inpAddr = document.getElementById("crA").value;
+  const URL = "https://crud-express-mongodb-basic.herokuapp.com/user";
+  if (!inpName || !inpEmail || !inpAddr) {
+    //if(inpName === ""||inpEmail === "" || inpAddr ==="") {}
+    alert("Input All Fields");
+  }
+  axios
+    .post(URL, {
+      name: inpName,
+      email: inpEmail,
+      address: inpAddr,
+    })
+    .then((response) => {
+      alert(`USER is added `);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      alert("Enter All fields Correctly Please!!");
+      console.log(error);
+    });
+    document.getElementById("crN").value = ""
+    document.getElementById("crE").value = ""
+    document.getElementById("crA").value = ""
+});
+
+Btnuser.addEventListener("click", () => {
+  let InpgetUser = document.getElementById("get_user").value;
+  document.getElementById("tbod").textContent = ""
+  axios
+    .get(`https://crud-express-mongodb-basic.herokuapp.com/user/${InpgetUser}`)
+    .then((response) => {
+      //console.log(response.data);
+      datauser = response.data;
+      //console.log(datauser);
+      let trUsers = document.createElement("tr");
+      let td1 = document.createElement("td");
+      let td2 = document.createElement("td");
+      let td3 = document.createElement("td");
+      td1.appendChild(document.createTextNode(datauser.name));
+      td2.appendChild(document.createTextNode(datauser.email));
+      td3.appendChild(document.createTextNode(datauser.address));
+      trUsers.appendChild(td1);
+      trUsers.appendChild(td2);
+      trUsers.appendChild(td3);
+      document.getElementById("tbod").appendChild(trUsers);
+    })
+    .catch((error) => {
+      alert(`${error}, User doesn't Exist`)
+      console.log(error);
+    });
+});
+
+
+ function EDIT(editid) {
+  let editNam = document.getElementById("Edna").value;
+  let editEmai = document.getElementById("Edem").value;
+  let editAddr = document.getElementById("EdAddr").value;
+  axios
+    .put(`https://crud-express-mongodb-basic.herokuapp.com/user/${editid}`, {
+      name: editNam,
+      email: editEmai,
+      address: editAddr,
+    })
+    .then((response) => {
+      alert('User Updated Successfully')
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  document.getElementById("Edna").value = ""
+  document.getElementById("Edem").value = ""
+  document.getElementById("EdAddr").value = ""
+}
+
+function DELETE(editid){
+  axios
+  .delete(`https://crud-express-mongodb-basic.herokuapp.com/user/${editid}`)
+  .then((response) => {
+    alert("User Deleted Succesfully")
+    console.log(response.data +" "+ response.status);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+
+BtnDel.addEventListener("click", () => {
+  const DeleID = document.getElementById("delid").value;
+  axios
+    .delete(`https://crud-express-mongodb-basic.herokuapp.com/user/${DeleID}`)
+    .then((response) => {
+      alert("User Deleted Succesfully")
+      console.log(response.data +" "+ response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+BtnDelAll.addEventListener("click", () => {
+  axios
+    .delete("https://crud-express-mongodb-basic.herokuapp.com/userdelall")
+    .then((response) => {
+      alert("All User Deleted Successfully")
+      console.log(response.data +" "+ response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
---
+++ server.mjs
@@ -0,0 +1,204 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+
+//                       <username>:<password>                                 <database_name>
+//const dburl = "mongodb+srv://faiq:faiqkhan@11nov21cluster1.lxgo4.mongodb.net/crud-mongodb?retryWrites=true&w=majority"          
+//mongoose.connect(dburl)  //this will get deprecation warning so we will add {useNewUrlParser: true, useUnifiedTopology:true} to remove the warnings
+//it will create a <database_name> of that name if the name doesn't exist.
+
+//                 put the url in enviornment variable named mongoURI
+mongoose.connect('mongodb+srv://faiq:faiqkhan@11nov21cluster1.lxgo4.mongodb.net/crud-mongodb?retryWrites=true&w=majority')  //using this one, it doesn't give out deprecating url warnings
+    .then(() => console.log("Connected to the Database"))
+    .catch(err => console.log(err));         
+ 
+//const Users = mongoose.model('Users', Schema)
+//created the schema in the arg itself
+const Users = mongoose.model('Users', {
+    name: String,
+    email: String,
+    address: String,
+    //_id: String
+});
+
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+//let users = [];  //now we are storing it in database.
+app.use(cors())
+app.use(express.json()) 
+app.use(morgan('short')) 
+
+
+
+app.use((req, res, next) => {
+    console.log('a request came', req.body);
+    next()
+})
+
+
+app.get('/users', (req,res) => {
+    //res.send(users)
+    Users.find({},(error, userS) => { //have to define empty object{} in order to get all the users in db
+        if (!error) {                  //if we wrote userS in if condition and no users are present than it would have given us empty array, which is truthy  , and else block execute
+            res.send(userS)
+        } else {
+            console.log(err);
+            res.status(404).send("error happened")
+        }
+    })
+})
+
+
+app.get('/user/:id', (req,res) => {
+    console.log(req.params.id)
+    Users.findOne({_id: req.params.id}, (err,user) => { //findbyId() is used to find by the _Id of mongodb directly, 
+        if(!err) {                                        //findOne() have it's first parameter as a object defining that _id mongodb id would be equal to parameter id, which we define
+            res.send(user)                            //findById(id) is almost* equivalent to findOne({ _id: id })
+            console.log(user);
+        } else {
+            console.log(err);
+            res.status(500).send('Error Happened')
+        }
+    })
+})
+
+
+app.post('/user', (req,res) => {
+    if (!req.body.name || !req.body.email || !req.body.address){
+        res.status(400).send('invalid data');
+    } else {
+        const newUser = new Users({  //new Users is a new model,we are making on user data
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address
+        });
+
+        newUser.save()     //.save() will save the model variable in database
+            .then((result) => {
+                res.send(result);
+                console.log(result);
+                console.log(req.params.id);
+            })
+            .catch(err => console.log(err));
+    }
+})
+
+
+
+app.put ('/user/:id', (req, res) => {
+    let updateUser = {}   //making an empty object to update the user from it's req.params.id in .find method
+    if (req.body.name) {
+        updateUser.name = req.body.name
+    } 
+    if (req.body.email) {
+        updateUser.email = req.body.email
+    }
+    if (req.body.address) {
+        updateUser.address = req.body.address
+    }
+    //    findByIdAndUpdate(id, update, options, callback)
+    Users.findByIdAndUpdate(req.params.id, updateUser, {new:true}, (err, update) => {   //findByIdAndUpdate(id, ...) is equivalent to findOneAndUpdate({ _id: id }, ...).
+        if (!err) {                                     //{new:true //results the changed document, after the update has applied, default is returning the bfore doc from db}       
+            res.send(update)
+            console.log(update);
+        } else {
+            console.log(err);
+            res.status(404).send("User doesn't exist")
+        }                                                      
+    })
+
+    //below code is applicable in non database
+    /* if (users[req.params.id]) {
+        if (req.body.name) {
+            users[req.params.id].name = req.body.name
+        }
+        if (req.body.email) {
+            users[req.params.id].email = req.body.email
+        }
+        if (req.body.address) {
+            users[req.params.id].address = req.body.address
+        }
+
+        res.send(users[req.params.id])
+
+    } else {
+        res.status(404).send('user not found');
+    } */
+})
+
+
+app.delete('/user/:id', (req,res) => {
+    Users.findOneAndDelete({_id: req.params.id}, (err,del) => {
+        if (!err) {
+            res.send('USER deleted')
+            console.log(del);
+        } else {
+            console.log(err);
+            res.status(500).send("User not found/Error Happened")
+        }
+    })
+    //below code is  applicable in non database
+   /*  if (users[req.params.id]) {
+        users[req.params.id] = {}
+        res.send('user deleted')
+    } else {
+        res.status(404).send('user not found');
+    } */
+})
+
+app.delete('/userdelall', (req,res) => {
+    Users.remove()
+        .then(() => {
+            res.send("All Users Deleted")
+        })
+        .catch(() => {
+            res.status(404).send("No Users Exist")
+        })
+        //below code is  applicable in non database
+    /* if (users) {
+        users = []
+        res.send('All user deleted')
+    } else {
+        res.status(404).send('users doesn"t exists');
+    } */
+})
+
+app.get('/home', (req, res) => {
+    res.send('here is your home')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hi this is a nice little hello server')
+})
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
+
+
+
+
+
+
+
+//mongodb is a nosql database, mongodb atlas is a nosql database on cloud.
+//moongose is an ODM library - object document mapping library
+    //allows us to create simple database models, which have methods like
+    //.get() , .findByID(), deleteOne() etc .
+//In order to work with mongodb and moongose we create schemas and models
+
+//schema: defines the structure of a type of data / document stored in a database collection
+        //-properties & property types that should have and etc etc.
+        //e.g let's say a,
+           //User Schema will contain : 
+               //name (String), required  
+               //age (String), required
+               //bio (String), required
+            //string represents the type of data written
+
+//models: After creation of schema we create a model based on schema, models allows us to communicate with databse collections
+        //If we create a User Schema, we can use the instant and static method like save, delete or update to edit the database collection
